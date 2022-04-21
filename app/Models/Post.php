018271764d7588342e6f31db5cef2042a165a8df
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-04-20 23:47:51 -0300

    add a Yaml Front Matter parser and add metadata to posts and uses collections

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,30 +2,74 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title   = $title;
+        $this->excerpt = $excerpt;
+        $this->date    = $date;
+        $this->body    = $body;
+        $this->slug    = $slug;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
+        return collect(File::files(resource_path("posts/")))
+            ->map(fn ($file)     => YamlFrontMatter::parseFile($file))
+            ->map(fn ($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
 
-        return array_map(fn ($file) => $file->getContents(), $files);
+        // same as above. Collections is the Laravel way
 
-        // again same as above. training the new syntax
+    // $posts = array_map(function ($file) {
+    //     $document = YamlFrontMatter::parseFile($file);
 
-        // return array_map(function ($file) {
-        //     return $file->getContents();
-        // }, $files);
+    //     return new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->body(),
+    //         $document->slug
+    //     );
+    // }, $files);
+
+    // If we want to loop and build an array we probably want to use array_map
+
+    // $posts = [];
+
+    // foreach ($files as $file) {
+    //     $document = YamlFrontMatter::parseFile($file);
+    //     $posts[]  = new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->body(),
+    //         $document->slug
+    //     );
+    // }}
     }
 
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
