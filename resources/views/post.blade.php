238ddeb3e246493fb34229d4ac0ea6f6aadc339b
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-05-03 21:56:35 -0300

    use blade component for layout and refactor posts route

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-
-<body>
     <article>
         <h1>
-            <?= $post->title; ?>
+            {{ $post->title }}
         </h1>
 
         <div>
-            <?= $post->body; ?>
+            {!! $post->body !!}
         </div>
     </article>
 
     <a href="/">Go back</a>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-
-<body>
-    <?php foreach ($posts as $post):?>
+    @foreach ($posts as $post)
     <article>
         <h1>
-            <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
             </a>
         </h1>
         <div>
-            <?= $post->excerpt; ?>
+            {{ $post->excerpt }}
         </div>
     </article>
-    <?php endforeach;?>
-</body>
-
-</html>
+    @endforeach
+</x-layout>
