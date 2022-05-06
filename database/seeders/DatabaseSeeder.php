6e8b64e4df4498a99e0b56e3c5e3ca67686c1021
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-05-05 23:27:15 -0300

    add missing relationships and seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        Post::create([
+            'user_id'     => $user->id,
+            'category_id' => $family->id,
+            'title'       => 'My Family Post',
+            'slug'        => 'my-family-post',
+            'excerpt'     => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body'        => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam vitae voluptatem quas maiores incidunt, consequuntur enim, ullam dolor officia explicabo fuga voluptatibus quidem cumque. Quaerat eum facilis labore doloribus magni.</p>',
+        ]);
+
+        Post::create([
+            'user_id'     => $user->id,
+            'category_id' => $work->id,
+            'title'       => 'My Work Post',
+            'slug'        => 'my-work-post',
+            'excerpt'     => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body'        => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam vitae voluptatem quas maiores incidunt, consequuntur enim, ullam dolor officia explicabo fuga voluptatibus quidem cumque. Quaerat eum facilis labore doloribus magni.</p>',
+        ]);
     }
 }
