c6189ac81e7324a9e0a881cf09ea0ba5a2637901
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-05-05 21:37:43 -0300

    add categories to posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,10 @@
             {{ $post->title }}
         </h1>
 
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
+
         <div>
             {!! $post->body !!}
         </div>
