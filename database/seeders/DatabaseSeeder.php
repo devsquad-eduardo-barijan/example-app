5d12bfff96da9f3dc9c71aa8c16900091ad0b16b
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-05-05 23:57:28 -0300

    add factories for categories and posts and update seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,43 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'John Doe',
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
-            'user_id'     => $user->id,
-            'category_id' => $family->id,
-            'title'       => 'My Family Post',
-            'slug'        => 'my-family-post',
-            'excerpt'     => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body'        => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam vitae voluptatem quas maiores incidunt, consequuntur enim, ullam dolor officia explicabo fuga voluptatibus quidem cumque. Quaerat eum facilis labore doloribus magni.</p>',
-        ]);
-
-        Post::create([
-            'user_id'     => $user->id,
-            'category_id' => $work->id,
-            'title'       => 'My Work Post',
-            'slug'        => 'my-work-post',
-            'excerpt'     => '<p>Lorem ipsum dolar sit amet.</p>',
-            'body'        => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam vitae voluptatem quas maiores incidunt, consequuntur enim, ullam dolor officia explicabo fuga voluptatibus quidem cumque. Quaerat eum facilis labore doloribus magni.</p>',
+        Post::factory(5)->create([
+            'user_id' => $user->id,
         ]);
     }
 }
