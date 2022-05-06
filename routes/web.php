64f7cd17f12b47dec58f08021a2376a071d48532
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-05-06 00:33:49 -0300

    eager load relatioships of posts by default

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
     });
 
     return view('posts', [
-        'posts' => Post::latest()->with(['category', 'author'])->get(),
+        'posts' => Post::latest()->get(),
     ]);
 });
 
