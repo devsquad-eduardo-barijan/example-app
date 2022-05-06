cb9ba1b44ef789c5c8db53b661dafd58094f8a52
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-05-06 00:21:39 -0300

    add authors to posts and authors page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,7 @@ Route::get('/', function () {
     });
 
     return view('posts', [
-        'posts' => Post::latest()->with('category')->get(),
+        'posts' => Post::latest()->with(['category', 'author'])->get(),
     ]);
 });
 
@@ -24,5 +25,11 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
     ]);
 });
