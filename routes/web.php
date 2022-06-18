3255341c38277e7594c7e8d9e32a73f1d39ffb7b
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-06-18 02:35:30 -0300

    extract dropdown to its own component

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,14 @@
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    DB::listen(function ($query) {
-        logger($query->sql, $query->bindings);
-    });
-
     return view('posts', [
         'posts'      => Post::latest()->get(),
         'categories' => Category::all(),
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -30,7 +25,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories'      => Category::all(),
     ]);
-});
+})->name('category');
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
